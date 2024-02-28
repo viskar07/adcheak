ae503162d108ea2eefad67cc4bda8b2db03ba8c0
Viskar.code <viskar.code@gmail.com>
2024-02-28 14:24:00 +0530

    Title Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@/components/shared/ThemeProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title:'InstaDl',
+  title:'InstaDL',
   description: "InstaDL is an online web tool that helps you download Instagram Videos, Photos, Reels, and IGTV. InstaDL is designed to be easy to use on any device, such as a mobile phone, tablet, or computer.",
 };
 
